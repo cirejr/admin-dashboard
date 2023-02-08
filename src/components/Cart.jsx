f2044f189daf89d9d5b17235c0973144fbd2f1ce
unknown <juniorcireba@gmail.com>
2023-02-08 00:13:43 +0000

    cart done!

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { MdOutlineCancel } from 'react-icons/md';
 
+import { Button } from './index'
 import { cartData } from '../data/dummy'
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Cart = () => {
+
+	const { setIsClicked, currentColor }= useStateContext()
+
+
   return (
 	<div className="bg-half-transparent w-screen fixed nav-item right-0 top-0">
 		<div className="bg-white dark:bg-secondary-dark dark:text-white h-screen float-right">
@@ -13,7 +18,7 @@ const Cart = () => {
 				<button
 					type='button'
 					className="cursor-pointer hover:drop-shadow-xl hover:bg-light-gray"
-					onClick={()=>{}}
+					onClick={() => setIsClicked(false)}
 				>
 					<MdOutlineCancel />
 				</button>
@@ -27,7 +32,7 @@ const Cart = () => {
 							<img 
 								src={item.image} 
 								alt={item.name}
-								className="w-16 rounded-xl"	
+								className="w-20 rounded-xl"	
 							/>
 							<div>
 								<p className='font-semibold'>{item.name}</p>
@@ -35,7 +40,24 @@ const Cart = () => {
 								<p>{item.price}</p>
 							</div>
 						</div>)
-					})}
+					})} 
+			</div>
+			<div className='p-5 flex flex-col gap-2'>
+				<div className="flex justify-between items-center">
+					<p className="text-gray-400">Sub total</p>
+					<p className="font-semibold">$890</p>
+				</div>
+				<div className="flex justify-between items-center mb-2">
+					<p className="text-gray-400">Total</p>
+					<p className="font-semibold">$890</p>
+				</div>
+				<Button
+					bgColor={currentColor}
+					color="white"
+					size="lg"
+					borderRadius="10px"
+					text="Place Order"
+				/>
 			</div>
 		</div>
 	</div>
